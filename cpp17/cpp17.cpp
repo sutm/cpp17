2bc6347c89cd8085c7405644824a2ee3d96d14f2
tunmiang <sutm@live.com>
2018-05-22 23:28:04 +0800

    change project to C++17, test std::reduce

--- cpp17/cpp17.cpp
+++ cpp17/cpp17.cpp
@@ -5,14 +5,42 @@
 #include <array>
 #include <numeric>
 #include <iostream>
+#include <execution>
+#include <chrono>
 
 using namespace std;
 
-int main()
+void test_reduce_parallel()
 {
 	array<int, 10> data = { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 };
-	auto total = std::reduce(begin(data), end(data));
-	cout << "sum: " << total << endl;
+
+	auto start = chrono::steady_clock::now();
+	auto total = reduce(execution::par_unseq, begin(data), end(data), 0);
+	auto end = chrono::steady_clock::now();
+	auto elapsed = end - start;
+
+	cout << "sum parallel: " << total << "(" << chrono::duration<double, milli>(elapsed).count() << " ms)" << endl;
+}
+
+void test_reduce_sequential()
+{
+	array<int, 10> data = { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+
+	auto start = chrono::steady_clock::now();
+	auto total = reduce(begin(data), end(data), 0);
+	auto end = chrono::steady_clock::now();
+	auto elapsed = end - start;
+
+	cout << "sum sequential: " << total << "(" << chrono::duration<double, milli>(elapsed).count() << " ms)" << endl;
+}
+
+int main()
+{
+	test_reduce_sequential();
+	test_reduce_parallel();
+
     return 0;
 }
+
+
 
